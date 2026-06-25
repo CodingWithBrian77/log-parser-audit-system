b0e7e0aac2de2790bf2e07b32a0612688e295165
CodingWithBrian77 <codingwithbrian77@gmail.com>
2026-06-25 12:56:22 -0400

    Finished INSERT INTO statements and polished each line by stripping each line through the reading loop. Polished CREATE TABLE statement.

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import sqlite3
 import re
 
 data_file = open("mock_logs.txt")
-for line in data_file:
-    if "ERROR" in line or "WARNING" in line:
-        print(line)
 
 database_file = sqlite3.connect("log_vault.db")
 cursor = database_file.cursor()
-cursor.execute(
-    "CREATE TABLE log_vault (" \
-    " log_id INT AUTO_INCREMENT PRIMARY KEY," \
-    " log_timestamp VARCHAR(23)," \
-    " log_status VARCHAR(10)," \
-    " log_message VARCHAR(100)" \
-    ")"
-    )
+cursor.execute("""
+        CREATE TABLE IF NOT EXISTS log_vault (
+            log_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            log_timestamp TEXT,
+            log_status TEXT,
+            log_message TEXT
+        )
+    """)
 
+for line in data_file:
+    if "ERROR" in line or "WARNING" in line:
+        clean_line = line.strip()
+
+        cursor.execute(
+            "INSERT INTO log_vault(log_timestamp, log_status, log_message) VALUES (?, ?, ?)", (clean_line.split(" - ", 2))
+        )
+
+database_file.commit()
+database_file.close()
